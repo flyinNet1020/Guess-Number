3be68aa60a455055b0681cc899245f2afd5bd976
Thomas Liao <flyinNet1020@gmail.com>
2021-06-20 16:24:11 +0800

    basic version

---
+++ r.py
@@ -0,0 +1,14 @@
+import random
+r = random.randint(1, 100) # Generate a random number
+print ('嗨! 我們來玩猜數字吧！')
+
+while True:
+	guess_N = input ('請猜一個1~100的數字:')
+	guess_N = int(guess_N)
+	if r < guess_N:
+		print ('Oh~你猜錯了，答案比', guess_N, '要小')
+	elif r > guess_N:
+		print ('Oh~你猜錯了，答案比', guess_N, '要大')
+	else:
+		print ('哈！ 你猜對了，恭喜！ 答案就是', guess_N)
+		break
